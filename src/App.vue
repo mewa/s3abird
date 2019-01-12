bf2a7e3939182dc7bd05d4eb02d50eee47df2130
mewa <marcin.k.chmiel@gmail.com>
2019-01-12 17:33:33 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
 <div>
   <Settings></Settings>
-  <div class="container bg-light my-2 my-md-5 py-4 rounded">
+  <div class="container bg-light mt-md-5 my-2 py-4 rounded">
     <keep-alive include="EmailList">
       <router-view></router-view>
     </keep-alive>
   </div>
+  <footer class="container d-flex justify-content-between my-2">
+    <span>Made with <i class="fas fa-heart"></i> by <a href="https://marcinchmiel.com">Marcin Chmiel</a></span>
+    <a href="https://github.com/mewa/s3abird"><i class="fab fa-github"></i> Contribute</a>
+  </footer>
 </div>
 </template>
 
@@ -19,3 +23,20 @@ module.exports = {
     }
 };
 </script>
+
+<style lang="scss" scoped>
+footer {
+    font-size: 0.875rem;
+
+    color: rgba(#fff, .4);
+
+    a {
+        &:hover {
+            color: rgba(#fff, 7);
+        }
+
+        color: inherit;
+        text-decoration: none;
+    }
+}
+</style>
